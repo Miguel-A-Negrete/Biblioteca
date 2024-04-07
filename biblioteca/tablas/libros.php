fff59783bc2561f217e599e60e8084a3fe36c262
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-07 15:09:10 -0500

    Update libros.php

--- biblioteca/tablas/libros.php
+++ biblioteca/tablas/libros.php
@@ -20,11 +20,28 @@ include_once '../conexion/Conexion.php';
         return $rows;
     }
 
-	  // ingresa una autor a la database
+	  // ingresa un libro a la database
 	  public function insert($Titulo, $AutorID, $EditorialID, $GeneroID, $FechaPublicacion, $ISBN, $Precio, $CantidadStock, $Paginas, $Idioma) {
 	    $sql = 'INSERT INTO libros (Titulo, AutorID, EditorialID, GeneroID, FechaPublicacion, ISBN, Precio, CantidadStock, Paginas, Idioma) VALUES (:Titulo, :AutorID, :EditorialID, :GneroID, :FechaPublicacion, :ISBN, :Precio, :CantidadStock, :Paginas, :Idioma)';
 	    $stmt = $this->conn->prepare($sql);
 	    $stmt->execute(['Titulo' => $Titulo, 'AutorID' => $AutorID, 'EditorialID' => $EditorialID, 'GeneroID' => $GeneroID, 'FechaPublicacion' => $FechaPublicacion, 'ISBN' => $ISBN, 'Precio' => $Precio, 'CantidadStock' => $CantidadStock, 'Paginas' => $Paginas, 'Idioma' => $Idioma]);
+	    return true;
+	  }
+	}
+
+		  // actualiza los datos del libroautor
+	  public function update($Titulo, $AutorID, $EditorialID, $GeneroID, $FechaPublicacion, $ISBN, $Precio, $CantidadStock, $Paginas, $Idioma, $LibroID) {
+	    $sql = 'UPDATE libros SET Titulo = :Titulo, AutorID = :AutorID, EditorialID = :EditorialID, GeneroID = :GeneroID, FechaPublicacon = :FechaPublicacion, ISBN = :ISBN, Precio = :Precio, CantidadStock = :CantidadStock, Paginas = :Paginas, Idioma = :Idioma  WHERE LibroID = :LibroID';
+	    $stmt = $this->conn->prepare($sql);
+	    $stmt->execute(['Titulo' => $Titulo, 'AutorID' => $AutorID, 'EditorialID' => $EditorialID, 'GeneroID' => $GeneroID, 'FechaPublicacion' => $FechaPublicacion, 'ISBN' => $ISBN, 'Precio' => $Precio, 'CantidadStock' => $CantidadStock, 'Paginas' => $Paginas, 'Idioma' => 'Idioma', 'LibroID' => $LibroID]);
+	    return true;
+	  }
+
+	  // Elimina a un autor de la base de datos
+	  public function delete($LibroID) {
+	    $sql = 'DELETE FROM libros WHERE LibroID = :LibroID';
+	    $stmt = $this->conn->prepare($sql);
+	    $stmt->execute(['LibroID' => $LibroID]);
 	    return true;
 	  }
 	}
