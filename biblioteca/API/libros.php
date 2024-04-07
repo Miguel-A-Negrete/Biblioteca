e3dfb09ebd6423708bad939305cc99db30a24dde
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-07 15:14:49 -0500

    Update libros.php

---
+++ biblioteca/API/libros.php
@@ -0,0 +1,95 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Content-Type: application/json');
+
+include_once '../tablas/libros.php';
+
+$Libro = new Database();
+$api = $_SERVER['REQUEST_METHOD'];
+$LibroID = intval($_GET['LibroID'] ?? '');
+
+//obtiene uno o todos los libros de la DB
+if ($api == 'GET') {
+	if ($LibroID != 0) {
+		$data = $Libro->fetch($LibroID);
+	} else {
+		$data = $Libro->fetch();
+	}
+	echo json_encode($data);
+}
+
+//Agrega un libro a la DB
+if ($api == 'POST') {
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data, true);
+    if ($data !== null) {
+        $Titulo = $data['Titulo'] ?? '';
+        $AutorID = $data['AutorID'] ?? '';
+        $EditorialID = $data['EditroialID'] ?? '';
+        $GeneroID = $data['GeneroID'] ?? '';
+        $FechaPublicacion = $data['FechaPublicacion'] ?? '';
+        $ISBN = $data['ISBN'] ?? '';
+        $Precio = $data['Precio'] ?? '';
+        $CantidadStock = $data['CantidadStock'] ?? '';
+        $Paginas = $data['Paginas'] ?? '';
+        $Idioma = $data['Idioma'] ?? '';
+
+        if ($Autor->insert($Titulo, $AutorID, $EditorialID, $GeneroID, $FechaPublicacion, $ISBN, $Precio, $CantidadStock, $Paginas, $Idioma)) {
+            echo $Autor->message('Libro agregado de forma exitosa!', false);
+        } else {
+            echo $Autor->message('No se pudo agregar el libro!', true);
+        }
+    } else {
+      
+        echo $Autor->message('Error: JSON invalido', true);
+    }
+}
+
+
+//actualizar los datos del libro en la DB
+if ($api == 'PUT') {
+	parse_str(file_get_contents('php://input'), $post_input);
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data, true);
+    if ($data !== null) {
+        $Titulo = $data['Titulo'] ?? '';
+        $AutorID = $data['AutorID'] ?? '';
+        $EditorialID = $data['EditroialID'] ?? '';
+        $GeneroID = $data['GeneroID'] ?? '';
+        $FechaPublicacion = $data['FechaPublicacion'] ?? '';
+        $ISBN = $data['ISBN'] ?? '';
+        $Precio = $data['Precio'] ?? '';
+        $CantidadStock = $data['CantidadStock'] ?? '';
+        $Paginas = $data['Paginas'] ?? '';
+        $Idioma = $data['Idioma'] ?? '';
+
+
+	if ($Libro != null) {
+		if ($Libro->update($Titulo, $AutorID, $EditorialID, $GeneroID , $FechaPublicacion, $ISBN, $Precio, $CantidadStock, $Paginas, $Idioma, $LibroID)) {
+			echo $Libro->message('Libro actualizado de forma exitosa!',false);
+		} else {
+			echo $Libro->message('No se pudo actualizar el libro!',true);
+		}
+	} else {
+		echo $Libro->message('Libro no encontrado!',true);
+	}
+}
+}
+
+
+//eliminar libro de la DB
+
+if ($api == 'DELETE') {
+	if ($LibroID != null) {
+		if ($Libro->delete($LibroID)) {
+			echo $Libro->message('Libro eliminado de forma exitosa!', false);
+		} else {
+			echo $Libro->message('No se pudo eliminar el libro!', true);
+		}
+	} else {
+		echo $Libro->message('Libro no encontrado!', true);
+	}
+}
+
