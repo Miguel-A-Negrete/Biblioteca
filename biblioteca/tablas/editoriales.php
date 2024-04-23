8ff5d2520e04776d2ef22fcfb7a62ad41f16d9ee
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-22 21:20:24 -0500

    Update editoriales.php

--- biblioteca/tablas/editoriales.php
+++ biblioteca/tablas/editoriales.php
@@ -1,44 +1,56 @@
 <?php
-	include_once '../conexion/Conexion.php';
+	include_once '../conexion/DB.php';
 
 	
-	class Database extends Config {
+	class Editorial {
+		private $tableName = 'editoriales';
+		private $connect;
+		private $EditorialID;
+		private $Nombre;
+		private $Direccion;
+		private $Telefono;
+		private $Email;
+		private $SitioWeb;
+	
+		public function __construct(){
+			$this->connect = DB::getInstance();
+		}
 	  // Obtiene todos o una sola editorial
-      public function fetch($EditorialID = 0) {
-        $sql = 'SELECT * FROM editoriales';
+      public function get($EditorialID = 0) {
+        $sql = 'SELECT * FROM ' . $this->tableName;
         if ($EditorialID != 0) {
             $sql .= ' WHERE EditorialID = :EditorialID';
         }
-        $stmt = $this->conn->prepare($sql);
+        $stmt = $this->connect->prepare($sql);
         if ($EditorialID != 0) {
             $stmt->execute(['EditorialID' => $EditorialID]);
         } else {
             $stmt->execute(); 
         }
-        $rows = $stmt->fetchAll();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $rows;
     }
 
 	  // ingresa una editorial a la database
 	  public function insert($Nombre, $Direccion, $Telefono, $Email, $SitioWeb) {
-	    $sql = 'INSERT INTO editoriales (Nombre, Direccion, Telefono, Email, SitioWeb) VALUES (:Nombre, :Direccion, :Telefono, :Email, :SitioWeb)';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'INSERT INTO ' . $this->tableName . '(Nombre, Direccion, Telefono, Email, SitioWeb) VALUES (:Nombre, :Direccion, :Telefono, :Email, :SitioWeb)';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['Nombre' => $Nombre, 'Direccion' => $Direccion, 'Telefono' => $Telefono, 'Email' => $Email, 'SitioWeb' => $SitioWeb]);
 	    return true;
 	  }
 
 	  // actualiza los datos de la editorial
 	  public function update($Nombre, $Direccion, $Telefono, $Email, $SitioWeb, $EditorialID) {
-	    $sql = 'UPDATE editoriales SET Nombre = :Nombre, Direccion = :Direccion, Telefono = :Telefono, Email = :Email, SitioWeb = :SitioWeb  WHERE EditorialID = :EditorialID';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'UPDATE ' . $this->tableName . ' SET Nombre = :Nombre, Direccion = :Direccion, Telefono = :Telefono, Email = :Email, SitioWeb = :SitioWeb  WHERE EditorialID = :EditorialID';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['Nombre' => $Nombre, 'Direccion' => $Direccion, 'Telefono' => $Telefono, 'Email' => $Email, 'SitioWeb' => $SitioWeb, 'EditorialID' => $EditorialID]);
 	    return true;
 	  }
 
 	  // Elimina a una editorial de la base de datos
 	  public function delete($EditorialID) {
-	    $sql = 'DELETE FROM editoriales WHERE EditorialID = :EditorialID';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'DELETE FROM ' . $this->tableName . ' WHERE EditorialID = :EditorialID';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['EditorialID' => $EditorialID]);
 	    return true;
 	  }
