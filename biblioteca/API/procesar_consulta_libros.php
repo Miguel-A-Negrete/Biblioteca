47b80a9384c7e90ba94cafd4aa4df3ac5bde0b94
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-22 21:18:32 -0500

    Update procesar_consulta_libros.php

--- biblioteca/API/procesar_consulta_libros.php
+++ biblioteca/API/procesar_consulta_libros.php
@@ -1,32 +1,13 @@
 <?php
-// Configuración de la conexión a la base de datos
-$host = 'localhost';
-$dbname = 'taller1';
-$username = 'root';
-$password = '';
+include_once '../tablas/libros.php';
+include_once '../Views/view.php';
 
-// Obtener el ID del autor de los parámetros de la solicitud
-$autor_id = isset($_GET['autor_id']) ? $_GET['autor_id'] : null;
+    $Libro = new Libro();
+    $api = $_SERVER['REQUEST_METHOD'];
+    $AutorID = intval($_GET['AutorID'] ?? '');
 
-try {
-    // Conexión a la base de datos usando PDO
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Consulta para obtener los registros de libros relacionados con el autor
-    $query = "SELECT autores.AutorID AS IdAutor, autores.Nombre AS NombreAutor, autores.Apellido AS ApellidoAutor, libros.Titulo, libros.FechaPublicacion, libros.ISBN
-    FROM autores
-    INNER JOIN libros ON autores.AutorID = libros.AutorID
-    WHERE autores.AutorID = :autor_id;
-    ";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute(['autor_id' => $autor_id]);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Devolver los resultados en formato JSON
-    echo json_encode($result);
-} catch (PDOException $e) {
-    // Manejar errores de conexión o consulta
-    echo json_encode(['error' => 'Error de conexión o consulta']);
-}
+    if ($api == 'GET') {
+        $data = $Libro->getJoin($AutorID);
+        View::returnJSON($data);
+    }
 ?>
