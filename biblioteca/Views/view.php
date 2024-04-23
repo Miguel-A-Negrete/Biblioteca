85371f3f1ec63624267314a3135821519036180f
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-22 21:16:10 -0500

    Add files via upload

---
+++ biblioteca/Views/view.php
@@ -0,0 +1,15 @@
+<?php
+
+    class View{
+        public static function returnJSON($data){
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        }
+
+        public static function returnHTML($data){
+            header('Content-Type: text/html');
+            echo json_encode($data);
+        }
+    }
+
+?>
