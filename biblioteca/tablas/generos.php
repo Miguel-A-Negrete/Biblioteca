53055aa46fd15c1495922170fb378630bd20372a
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-22 21:20:43 -0500

    Update generos.php

--- biblioteca/tablas/generos.php
+++ biblioteca/tablas/generos.php
@@ -1,96 +1,58 @@
-<<<<<<< HEAD
 <?php
-	include_once '../conexion/Conexion.php';
+	include_once '../conexion/DB.php';
 
 	
-	class Database extends Config {
+	class Genero {
+		private $tableName = 'generos';
+		private $connect;
+		private $GeneroID;
+		private $NombreGenero;
+		private $Descripcion;
+		private $Popularidad;
+		private $CantidadLibros;
+		private $FechaCreacion;
+	
+		public function __construct(){
+			$this->connect = DB::getInstance();
+		}
 	  // Obtiene todos o un solo genero
-      public function fetch($GeneroID = 0) {
-        $sql = 'SELECT * FROM generos';
+      public function get($GeneroID = 0) {
+        $sql = 'SELECT * FROM ' . $this->tableName;
         if ($GeneroID != 0) {
             $sql .= ' WHERE GeneroID = :GeneroID';
         }
-        $stmt = $this->conn->prepare($sql);
+        $stmt = $this->connect->prepare($sql);
         if ($GeneroID != 0) {
             $stmt->execute(['GeneroID' => $GeneroID]);
         } else {
             $stmt->execute(); 
         }
-        $rows = $stmt->fetchAll();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $rows;
     }
 
 	  // ingresa una genero a la database
 	  public function insert($NombreGenero, $Descripcion, $Popularidad, $CantidadLibros, $FechaCreacion) {
-	    $sql = 'INSERT INTO generos (NombreGenero, Descripcion, Popularidad, CantidadLibros, FechaCreacion) VALUES (:NombreGenero, :Descripcion, :Popularidad, :CantidadLibros, :FechaCreacion)';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'INSERT INTO ' . $this->tableName . '(NombreGenero, Descripcion, Popularidad, CantidadLibros, FechaCreacion) VALUES (:NombreGenero, :Descripcion, :Popularidad, :CantidadLibros, :FechaCreacion)';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['NombreGenero' => $NombreGenero, 'Descripcion' => $Descripcion, 'Popularidad' => $Popularidad, 'CantidadLibros' => $CantidadLibros, 'FechaCreacion' => $FechaCreacion]);
 	    return true;
 	  }
 
 	  // actualiza los datos del genero
 	  public function update($NombreGenero, $Descripcion, $Popularidad, $CantidadLibros, $FechaCreacion,$GeneroID) {
-	    $sql = 'UPDATE generos SET NombreGenero = :NombreGenero, Descripcion = :Descripcion, Popularidad = :Popularidad, CantidadLibros   = :CantidadLibros, FechaCreacion = :FechaCreacion  WHERE GeneroID = :GeneroID';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'UPDATE ' . $this->tableName . ' SET NombreGenero = :NombreGenero, Descripcion = :Descripcion, Popularidad = :Popularidad, CantidadLibros   = :CantidadLibros, FechaCreacion = :FechaCreacion  WHERE GeneroID = :GeneroID';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['NombreGenero' => $NombreGenero, 'Descripcion' => $Descripcion, 'Popularidad' => $Popularidad, 'CantidadLibros' => $CantidadLibros, 'FechaCreacion' => $FechaCreacion, 'GeneroID' => $GeneroID]);
 	    return true;
 	  }
 
 	  // Elimina a un genero de la base de datos
 	  public function delete($GeneroID) {
-	    $sql = 'DELETE FROM generos WHERE GeneroID = :GeneroID';
-	    $stmt = $this->conn->prepare($sql);
+	    $sql = 'DELETE FROM ' . $this->tableName . ' WHERE GeneroID = :GeneroID';
+	    $stmt = $this->connect->prepare($sql);
 	    $stmt->execute(['GeneroID' => $GeneroID]);
 	    return true;
 	  }
 	}
 
-=======
-<?php
-	include_once '../conexion/Conexion.php';
-
-	
-	class Database extends Config {
-	  // Obtiene todos o un solo genero
-      public function fetch($generoID = 0) {
-        $sql = 'SELECT * FROM generos';
-        if ($generoID != 0) {
-            $sql .= ' WHERE generoID = :generoID';
-        }
-        $stmt = $this->conn->prepare($sql);
-        if ($generoID != 0) {
-            $stmt->execute(['generoID' => $generoID]);
-        } else {
-            $stmt->execute(); 
-        }
-        $rows = $stmt->fetchAll();
-        return $rows;
-    }
-
-	  // ingresa una genero a la database
-	  public function insert($NombreGenero, $Descripcion, $Popularidad, $CantidadLibros, $FechaCreacion) {
-	    $sql = 'INSERT INTO generos (NombreGenero, Descripcion, Popularidad, CantidadLibros, FechaCreacion) VALUES (:NombreGenero, :Descripcion, :Popularidad, :CantidadLibros, :FechaCreacion)';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['NombreGenero' => $NombreGenero, 'Descripcion' => $Descripcion, 'Popularidad' => $Popularidad, 'CantidadLibros' => $CantidadLibros, 'FechaCreacion' => $FechaCreacion]);
-	    return true;
-	  }
-
-	  // actualiza los datos del genero
-	  public function update($NombreGenero, $Descripcion, $Popularidad, $CantidadLibros, $FechaCreacion) {
-	    $sql = 'UPDATE generos SET NombreGenero = :NombreGenero, Descripcion = :Descripcion, Popularidad = :Popularidad, CantidadLibros   = :CantidadLibros, FechaCreacion = :FechaCreacion  WHERE generoID = :generoID';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['NombreGenero' => $NombreGenero, 'Descripcion' => $Descripcion, 'Popularidad' => $Popularidad, 'CantidadLibros' => $CantidadLibros, 'FechaCreacion' => $FechaCreacion, 'generoID' => $generoID]);
-	    return true;
-	  }
-
-	  // Elimina a un genero de la base de datos
-	  public function delete($generoID) {
-	    $sql = 'DELETE FROM generos WHERE generoID = :generoID';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['genero' => $generoID]);
-	    return true;
-	  }
-	}
-
->>>>>>> e3dfb09ebd6423708bad939305cc99db30a24dde
-?>
