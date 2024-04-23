9efc4c37068ea7cf6830312e9c80eb84bccaf849
Jose Morales <149639682+Jose47Morales@users.noreply.github.com>
2024-04-22 21:20:07 -0500

    Update autores.php

--- biblioteca/tablas/autores.php
+++ biblioteca/tablas/autores.php
@@ -1,47 +1,59 @@
 <?php
-	include_once '../conexion/Conexion.php';
+	include_once '../conexion/DB.php';
 
+	class Autor{
+		private $tableName = 'autores';
+		private $connect;
+		private $AutorID;
+		private $Nombre;
+		private $Apellido;
+		private $Bio;
+		private $FechaNacimiento;
+		private $Nacionalidad;
 	
-	class Database extends Config {
-	  // Obtiene todos o un solo autor
-      public function fetch($AutorID = 0) {
-        $sql = 'SELECT * FROM autores';
-        if ($AutorID != 0) {
-            $sql .= ' WHERE AutorID = :AutorID';
-        }
-        $stmt = $this->conn->prepare($sql);
-        if ($AutorID != 0) {
-            $stmt->execute(['AutorID' => $AutorID]);
-        } else {
-            $stmt->execute(); 
-        }
-        $rows = $stmt->fetchAll();
-        return $rows;
-    }
+		public function __construct(){
+			$this->connect = DB::getInstance();
+		}
+	
+		//Seleccionar autores
+		public function get($AutorID = 0){
+			$sql = 'SELECT * FROM ' . $this->tableName;
+			if ($AutorID != 0) {
+				$sql .= ' WHERE AutorID = :AutorID';
+			}
+			$stmt = $this->connect->prepare($sql);
+			if ($AutorID != 0) {
+				$stmt->execute(['AutorID' => $AutorID]);
+			} else {
+				$stmt->execute(); 
+			}
+			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			return $rows;
+		}
 
-	  // ingresa una autor a la database
-	  public function insert($Nombre, $Apellido, $Bio, $FechaNacimiento, $Nacionalidad) {
-	    $sql = 'INSERT INTO autores (Nombre, Apellido, Bio, FechaNacimiento, Nacionalidad) VALUES (:Nombre, :Apellido, :Bio, :FechaNacimiento, :Nacionalidad)';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['Nombre' => $Nombre, 'Apellido' => $Apellido, 'Bio' => $Bio, 'FechaNacimiento' => $FechaNacimiento, 'Nacionalidad' => $Nacionalidad]);
-	    return true;
-	  }
+		//Insertar autores
+		public function insert($Nombre, $Apellido, $Bio, $FechaNacimiento, $Nacionalidad) {
+			$sql = 'INSERT INTO ' . $this->tableName . '(Nombre, Apellido, Bio, FechaNacimiento, Nacionalidad) VALUES (:Nombre, :Apellido, :Bio, :FechaNacimiento, :Nacionalidad)';
+			$stmt = $this->connect->prepare($sql);
+			$stmt->execute(['Nombre' => $Nombre, 'Apellido' => $Apellido, 'Bio' => $Bio, 'FechaNacimiento' => $FechaNacimiento, 'Nacionalidad' => $Nacionalidad]);
+			return true;
+		  }
 
-	  // actualiza los datos del autor
-	  public function update($Nombre, $Apellido, $Bio,$FechaNacimiento,$Nacionalidad, $AutorID) {
-	    $sql = 'UPDATE autores SET Nombre = :Nombre, Apellido = :Apellido, Bio = :Bio, FechaNacimiento = :FechaNacimiento, Nacionalidad = :Nacionalidad  WHERE AutorID = :AutorID';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['Nombre' => $Nombre, 'Apellido' => $Apellido, 'Bio' => $Bio, 'FechaNacimiento' => $FechaNacimiento, 'Nacionalidad' => $Nacionalidad, 'AutorID' => $AutorID]);
-	    return true;
-	  }
-
-	  // Elimina a un autor de la base de datos
-	  public function delete($AutorID) {
-	    $sql = 'DELETE FROM autores WHERE AutorID = :AutorID';
-	    $stmt = $this->conn->prepare($sql);
-	    $stmt->execute(['AutorID' => $AutorID]);
-	    return true;
-	  }
+		  //Actualizar autores
+		  public function update($Nombre, $Apellido, $Bio,$FechaNacimiento,$Nacionalidad, $AutorID) {
+			$sql = 'UPDATE ' . $this->tableName . ' SET Nombre = :Nombre, Apellido = :Apellido, Bio = :Bio, FechaNacimiento = :FechaNacimiento, Nacionalidad = :Nacionalidad  WHERE AutorID = :AutorID';
+			$stmt = $this->connect->prepare($sql);
+			$stmt->execute(['Nombre' => $Nombre, 'Apellido' => $Apellido, 'Bio' => $Bio, 'FechaNacimiento' => $FechaNacimiento, 'Nacionalidad' => $Nacionalidad, 'AutorID' => $AutorID]);
+			return true;
+		  }
+	
+		  // Eliminar autores
+		  public function delete($AutorID) {
+			$sql = 'DELETE FROM ' . $this->tableName . ' WHERE AutorID = :AutorID';
+			$stmt = $this->connect->prepare($sql);
+			$stmt->execute(['AutorID' => $AutorID]);
+			return true;
+		  }
 	}
 
 ?>
